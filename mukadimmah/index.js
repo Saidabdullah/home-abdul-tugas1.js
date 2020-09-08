01c29e05ca9ceebab39f365255a2b810aa7cb4bc
kirito <Mohabdullahsaid@gmail.com>
2020-09-08 09:31:45 +0700

    tugas1.js

---
+++ mukadimmah/index.js
@@ -0,0 +1,60 @@
+// // document.write("hello word")
+
+// // console.log("mustang")
+
+
+// var diameter = 9
+// var pi = 3.14
+// var hasil = pi * 9
+// var jari = 4.5
+// var luas = jari*jari*pi
+
+
+// // 1 . diketahui panjang = 10 cm, lebar = 5 cm , dan tinggi = 8 cm. hitung lah luas permukaan balok dan volume balok sehingga menampilkan outpud pada web
+
+// // diketahui :
+// // p = ... cm
+// // l= ... cm
+// // t=... cm
+
+// // ditanya ?
+// // volume dari balok adalah ... cm3
+
+var panjang= 10
+var lebar= 5
+var tinggi= 8
+var volume = panjang * lebar * tinggi
+var br= "<br>"
+document.write('diketehui: <br> p= 10 cm <br> l = 5 cm <br> t = 8 cm <br>')
+document.write("ditanya? <br> volume dari balok adalah "+ volume +"cm3 <br>")
+
+// // volume balok dalam liter  .. 1
+
+var dalamliter = volume / 1000
+document.write("ditanya: <br> volume balok dalam liter adalah "+ dalamliter +"l <br>") 
+
+// // volume balok keika di perbesar 3 kali ... cm3
+
+var diperbesar = volume * 3
+document.write("ditanya: <br> volume balok ketika di perbesar 3 kali adalah "+ diperbesar +"cm3 <br>")
+
+// //  2. diketahui segitiga memiliki alas = 10 cm, tinggi = 12 cm. hitunglah luas segitiga ,dan memiliki keluaran output pada web sbb
+
+// // diketahui:
+// // alas=..cm,
+// // tinggi=..cm
+
+// //ditanya?
+// // luas segitiga adalah ... m2
+
+var alas = 10 
+var tinggi = 12
+var luassegitiga = 1 / 2 * alas * tinggi / 10000
+document.write("diketahui : <br> alas = 10 cm <br> tinggi = 12 cm <br> ditanya : <br> luas segita adalah "+ luassegitiga +"m2")
+
+
+
+
+
+
+
