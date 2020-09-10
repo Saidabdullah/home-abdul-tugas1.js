4b1d3f70e02b5e39309102f387e845071d046d36
kirito <Mohabdullahsaid@gmail.com>
2020-09-10 09:16:38 +0700

    materibelajar

---
+++ percabangan/percabangan.js
@@ -0,0 +1,108 @@
+//var jawaban = ("setuju atau tidak ?"):
+
+//document.write(jawaban):
+
+//var peringatan = alert("jangna di buka !"):
+
+//document.write(peringatan):
+
+//var name = prompt("masukan nama ?"):
+//document.write(name):
+
+// program hitung luas persegi panjang
+// var panjang = prompt("masukan panjang");
+// var lebar = prompt("masukan lebar");
+
+// var panjangint = parseInt(panjang);
+// var lebarint = paresint(lebar);
+
+// var hasil = panjangint * lebarint;
+
+// document.write("luas persegi adalah = + hasil");
+
+
+//program hewan kaki
+// var jumlah= prompt('masukan jumlah kaki hewan ?')
+// var angka = prompt("masukan angka dua?")
+
+// if (angka == "2") {
+//  //ouput
+//  document.write("angka dua benar")
+
+// else {
+//  //output
+//  document.write("bukan angka dua")
+//{
+
+//program hewan berkaki
+// var jumlah kaki = prompt("masukan jumlah kaki hewan ?");
+
+// if (jumlahkaki == "2") {
+//     document.write("bebek, angsa , ayam , burung");
+//     {else if {jumlahkaki == "4"} {
+
+
+
+
+
+
+
+
+//membuat fitur login sederhana 
+
+// var username = "siadabdullah";
+// var password = "12345";
+
+// var Inputusername = prompt("masukan username");
+// var Inputpassword = prompt("masukan password");
+
+// if (inputusername == username && inputpassword == password) {
+//     document.write("loginberhasil") ;
+// } else if (inputusername != username) {
+//     document.write("username salah");
+// } else if (inputpassword != password) {
+//     document.write("password salah"); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var nilai1 =prompt ('masukan nilai1')
+// var nilai2 =prompt ('masukan nilai2')
+// var hasil =prompt ('masukan tanda +, -, *, /')
+
+// var n1Int =parseInt(nilai1)
+// var n2Int =parseInt(nilai2)
+
+
+    
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ perulangan/perulangan.js
@@ -0,0 +1,5 @@
+// var hellowolrd = 10;
+// var hellowolrdInt = parseInt(hellowolrd);
+
+for (var n = 1 n n <=10; n++) {
+    console.log
